10cf7fca233b817348459a8c676b7260add81c6a
saravana-aiml <saravanakd444@gmail.com>
2026-01-31 18:11:35 +0530

    Add files via upload

---
+++ main.py.py
@@ -0,0 +1,15 @@
+print("Welcome to Brand Name Generator")
+
+city = input("What's the name of the city you grew up in? ")
+color = input("What's your favorite color? ")
+actor = input("Who's your favorite actor? ")
+actress = input("Who's your favorite actress? ")
+state = input("Which state are you from? ")
+
+print(f"""
+Once upon a time, there lived a man called {actor}.
+He was immortal and god-gifted.
+He loved a girl called {actress},
+but she was not interested in love.
+So she shifted to {state}.
+""")
